6de9f7e35073d86e31178ce6dd57bf571f68bc1b
junhwan <mynameisljh@naver.com>
2022-07-07 17:46:55 +0900

    이준환

--- dabomweb/src/main/java/com/dabom/service/AccountServiceImpl.java
+++ dabomweb/src/main/java/com/dabom/service/AccountServiceImpl.java
@@ -1,9 +1,19 @@
 package com.dabom.service;
 
+import com.dabom.dto.Member;
+import com.dabom.mapper.MemberMapper;
+
 public class AccountServiceImpl {
 
-	public void registerMember() {
+	private MemberMapper memberMapper;
+	
+	public void setMemberMapper(MemberMapper memberMapper) {
+		this.memberMapper = memberMapper;
+	}
+	
+	public void registerMember(Member member) {
 		
+		memberMapper.insertMember(member);
 		
 	}
 	
