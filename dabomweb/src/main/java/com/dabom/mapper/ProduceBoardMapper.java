451fe43bcc29863cac1605d70e64ccd67e33761d
shhtjk000@naver.com <shhtjk000@naver.com>
2022-07-07 17:45:25 +0900

    pom.xml 수정 이종경

--- dabomweb/src/main/java/com/dabom/mapper/ProduceBoardMapper.java
+++ dabomweb/src/main/java/com/dabom/mapper/ProduceBoardMapper.java
@@ -1,10 +1,12 @@
 package com.dabom.mapper;
 
+import org.apache.ibatis.annotations.Insert;
+
 import com.dabom.service.ProduceBoardService;
 
-public interface ProduceBoardMapper {
-	
-	
-	 void insertProdueceBoard(ProduceBoardService produceBoard);
+public interface ProduceBoardMapper {		
+	 
+	@Insert("insert")
+	void insertProdueceBoard(ProduceBoardService produceBoard);
 
 }
