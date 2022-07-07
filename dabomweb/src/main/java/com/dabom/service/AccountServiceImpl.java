0dfab1fd857a69f3ae547e9fb9635cd88cd4e175
junhwan <mynameisljh@naver.com>
2022-07-07 18:22:12 +0900

    0707 마지막 작업!

--- dabomweb/src/main/java/com/dabom/service/AccountServiceImpl.java
+++ dabomweb/src/main/java/com/dabom/service/AccountServiceImpl.java
@@ -3,13 +3,13 @@ package com.dabom.service;
 import com.dabom.dto.Member;
 import com.dabom.mapper.MemberMapper;
 
+import lombok.Setter;
+
 public class AccountServiceImpl {
 
+	@Setter
 	private MemberMapper memberMapper;
 	
-	public void setMemberMapper(MemberMapper memberMapper) {
-		this.memberMapper = memberMapper;
-	}
 	
 	public void registerMember(Member member) {
 		
