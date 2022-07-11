543596cd9a2325f986fc62263eed30333bcc32bf
JUNHWAN96 <mynameisljh@naver.com>
2022-07-11 23:10:47 +0900

    한거 별로 없음 이준환

--- dabomweb/src/main/java/com/dabom/controller/WebtoonController.java
+++ dabomweb/src/main/java/com/dabom/controller/WebtoonController.java
@@ -1,9 +1,21 @@
 package com.dabom.controller;
 
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.dabom.common.Util;
+import com.dabom.dto.WebtoonBoard;
+import com.dabom.dto.WebtoonBoardAttach;
+
 
 @Controller
 @RequestMapping(path= {"webtoon"})
@@ -26,9 +38,31 @@ public class WebtoonController {
 	}
 	
 	@PostMapping(path= {"/webtoonRegister"})
-		public String webtoonRegisterForm() {
+	public String webtoonRegisterForm(WebtoonBoard webtoonBoard, MultipartFile[] attach, HttpServletRequest req) {
 		
-		return null;
+		String uploadDir = req.getServletContext().getRealPath("/resources/upload-files");
+		
+		ArrayList<WebtoonBoardAttach> files = new ArrayList<>();
+		
+		for(MultipartFile uploadFile : attach) {
+			String userFileName = uploadFile.getOriginalFilename();
+			if(userFileName != null && userFileName.length() > 0) {
+				WebtoonBoardAttach f = new WebtoonBoardAttach();
+				String savedFileName = Util.makeUniqueFileName(userFileName);			
+				f.setUserFileName(userFileName);
+				f.setSavedFileName(savedFileName);
+				try {
+					File savedFile = new File(uploadDir, savedFileName);
+					uploadFile.transferTo(savedFile); // 파일 저장
+					files.add(f);
+				}catch(Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+		webtoonBoard.setFiles(files);
+		
+		return "redirect:webtoonList";
 	}
 	
 }
